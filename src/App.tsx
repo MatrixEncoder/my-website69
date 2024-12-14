53b958ed184e1bbf6ad7969d17f34b55bc2d4986
MatrixEncoder <155551275+MatrixEncoder@users.noreply.github.com>
2024-12-14 13:21:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
